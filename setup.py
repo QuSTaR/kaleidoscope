411f292a1093f81f67e9c0300e618d14c94db536
Paul Nation <nonhermitian@gmail.com>
2020-03-24 21:13:52 -0400

    add description

--- setup.py
+++ setup.py
@@ -1,8 +1,7 @@
 # -*- coding: utf-8 -*-
 
-# This code is part of Qiskit.
 #
-# (C) Copyright IBM 2017, 2019.
+# (C) Copyright IBM 2017, 2020.
 #
 # This code is licensed under the Apache License, Version 2.0. You may
 # obtain a copy of this license in the LICENSE.txt file in the root directory
@@ -11,20 +10,25 @@
 # Any modifications or derivative works of this code must retain this
 # copyright notice, and modified files need to carry a notice indicating
 # that they have been altered from the originals.
+"""Kaleidoscope
 
-"Setup for Kaleidoscope"
+Quantum visualization package.
+"""
 
 import os
 import sys
 import setuptools
 
-REQUIREMENTS = [
-]
+REQUIREMENTS = []
 
 PACKAGES = ['kaleidoscope']
 PACKAGE_DATA = {
     'kaleidoscope': ['version.txt']
 }
+
+DOCLINES = __doc__.split('\n')
+DESCRIPTION = DOCLINES[0]
+LONG_DESCRIPTION = "\n".join(DOCLINES[2:])
 
 VERSION_PATH = os.path.abspath(
     os.path.join(os.path.join(os.path.dirname(__file__), 'kaleidoscope', 'VERSION.txt')))
@@ -36,7 +40,8 @@ setuptools.setup(
     version=VERSION,
     packages=PACKAGES,
     cmake_source_dir='.',
-    description="kaleidoscope",
+    description=DESCRIPTION,
+    long_description = LONG_DESCRIPTION,
     url="",
     author="kaleidoscope Development Team",
     author_email="nonhermitian@gmail.com",
