8b8182d2c3143bfc3e06e95a659138f2ab811fd4
Paul Nation <nonhermitian@gmail.com>
2020-10-06 05:05:43 -0400

    add ability to plot pure state dm on qsphere

---
+++ kaleidoscope/test/no_qiskit/test_qsphere.py
@@ -0,0 +1,36 @@
+# -*- coding: utf-8 -*-
+
+# This code is part of Kaleidoscope.
+#
+# (C) Copyright IBM 2020.
+#
+# This code is licensed under the Apache License, Version 2.0. You may
+# obtain a copy of this license in the LICENSE.txt file in the root directory
+# of this source tree or at http://www.apache.org/licenses/LICENSE-2.0.
+#
+# Any modifications or derivative works of this code must retain this
+# copyright notice, and modified files need to carry a notice indicating
+# that they have been altered from the originals.
+
+"""Tests for Bloch routines"""
+
+import pytest
+import numpy as np
+from qiskit import QuantumCircuit
+from qiskit. quantum_info import DensityMatrix, partial_trace
+from kaleidoscope import qsphere
+from kaleidoscope.errors import KaleidoscopeError
+import kaleidoscope.qiskit
+
+
+def test_qsphere_bad_dm_input():
+    """Tests the qsphere raises when passed impure dm"""
+
+    qc = QuantumCircuit(3)
+    qc.h(0)
+    qc.cx(0,1)
+    qc.cx(1,2)
+    dm = DensityMatrix.from_instruction(qc)
+    pdm = partial_trace(dm, [0,1])
+    with pytest.raises(KaleidoscopeError):
+        assert qsphere(pdm)
