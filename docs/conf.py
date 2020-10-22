1a3b1a785f6759ab7f4a69668bd28b9a47ecd0d5
Paul Nation <nonhermitian@gmail.com>
2020-10-22 19:51:11 -0400

    add templates back

--- docs/conf.py
+++ docs/conf.py
@@ -66,7 +66,7 @@ extensions = [
     'jupyter_sphinx'
 ]
 html_static_path = ['_static']
-#stemplates_path = ['_templates']
+templates_path = ['_templates']
 html_css_files = ['gallery.css']
 
 exclude_patterns = ['_build', '**.ipynb_checkpoints']
