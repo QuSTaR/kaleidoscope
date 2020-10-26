6cb1b3b0fbea271d3eef273cd9527fc215f81b32
Paul Nation <nonhermitian@gmail.com>
2020-10-26 05:07:43 -0400

    underscore things not to be used

--- kaleidoscope/qiskit/backends/device_layouts.py
+++ kaleidoscope/qiskit/backends/device_layouts.py
@@ -21,17 +21,17 @@ import requests
 # Try remote first since can be more up to date.
 LAYOUTS = None
 try:
-    REMOTE_JSON_URL = "https://github.com/nonhermitian/ \
-                       ibm_quantum_system_layouts/raw/main/layouts.json"
-    req = requests.get(REMOTE_JSON_URL)
-    req.raise_for_status()
+    _REMOTE_JSON_URL = "https://github.com/nonhermitian/ \
+                        ibm_quantum_system_layouts/raw/main/layouts.json"
+    _req = requests.get(_REMOTE_JSON_URL)
+    _req.raise_for_status()
 except Exception:
     pass
 else:
-    LAYOUTS = req.json()
+    LAYOUTS = _req.json()
 
 if LAYOUTS is None:
-    LAYOUTS_DIR = os.path.dirname(os.path.realpath(__file__))
-    with open(LAYOUTS_DIR+"/layouts.json", 'r') as f:
+    _LAYOUTS_DIR = os.path.dirname(os.path.realpath(__file__))
+    with open(_LAYOUTS_DIR+"/layouts.json", 'r') as f:
         LAYOUTS = json.load(f)
     f.close()
