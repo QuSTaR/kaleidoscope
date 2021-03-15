2350222417deaefde497218a4be698196924198e
Paul Nation <nonhermitian@gmail.com>
2021-03-15 10:39:39 -0400

    lint again

--- kaleidoscope/interactive/histogram.py
+++ kaleidoscope/interactive/histogram.py
@@ -235,10 +235,10 @@ def probability_distribution(data, figsize=(None, None), colors=None,
     if scale == 'log':
         lower = np.min([min(item.values())/sum(item.values()) for item in data])
         lower = int(np.floor(np.log10(lower)))
-        fig.update_yaxes(type="log", range=[lower,0])
+        fig.update_yaxes(type="log", range=[lower, 0])
         fig.update_layout(yaxis=dict(tickmode='array',
-                          tickvals=[10**k for k in range(lower,1)],
-                          ticktext=["10<sup>{}</sup>".format(k) for k in range(lower,1)]
+                          tickvals=[10**k for k in range(lower, 1)],
+                          ticktext=["10<sup>{}</sup>".format(k) for k in range(lower, 1)]
                           ))
 
     fig.update_layout(xaxis_tickangle=-70,
