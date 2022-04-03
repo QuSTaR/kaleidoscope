108cc3a26909eb2b12e0d44521f875551e096722
Paul Nation <nonhermitian@gmail.com>
2022-04-03 07:00:53 -0400

    lint

--- kaleidoscope/qiskit/backends/device_layouts.py
+++ kaleidoscope/qiskit/backends/device_layouts.py
@@ -11,7 +11,7 @@
 # Any modifications or derivative works of this code must retain this
 # copyright notice, and modified files need to carry a notice indicating
 # that they have been altered from the originals.
-# pylint: disable=broad-except
+# pylint: disable=broad-except, unspecified-encoding
 
 """Device layout information."""
 import os
