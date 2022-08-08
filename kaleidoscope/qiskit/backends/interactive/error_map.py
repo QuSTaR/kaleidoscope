f3922b5dd4733221b0a25582e8afc7d9ac7b33b8
Paul Nation <nonhermitian@gmail.com>
2022-08-08 17:23:20 -0400

    lint

--- kaleidoscope/qiskit/backends/interactive/error_map.py
+++ kaleidoscope/qiskit/backends/interactive/error_map.py
@@ -11,6 +11,7 @@
 # Any modifications or derivative works of this code must retain this
 # copyright notice, and modified files need to carry a notice indicating
 # that they have been altered from the originals.
+# pylint: disable=consider-using-generator
 
 # This code is part of Qiskit.
 #
